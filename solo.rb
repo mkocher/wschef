f482fb415d0d0b2da4905c27a55cf14a150d751d
Matthew Kocher <kocher@gmail.com>
2010-09-24 17:03:14 -0700

    boxbuilder

--- solo.rb
+++
@@ -1,5 +0,0 @@
-## Recipies in later entries in the cookbook array override earlier entries 
-cookbooks  = [File.expand_path("cookbooks")]
-cookbooks << File.expand_path("../project_cookbook") if File.exists?("../project_cookbook")
-cookbooks << File.expand_path("../workstation_cookbook") if File.exists?("../workstation_cookbook")
-cookbook_path cookbooks
